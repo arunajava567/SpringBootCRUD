f6ce04eb46db8df484fc32a513d6b53229a5f011
aruna <arunajava567@gmail.com>
2020-04-17 11:17:12 +0530

    initial commit 1

---
+++ SpringBootApp1/src/main/java/com/cg/SpringBootApp1/App.java
@@ -0,0 +1,13 @@
+package com.cg.SpringBootApp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
